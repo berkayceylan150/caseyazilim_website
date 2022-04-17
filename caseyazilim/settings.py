3e97b5cef8b27a8ebf081562683ef45743367df6
Berkay CEYLAN <50266126+berkayceylan150@users.noreply.github.com>
2022-04-17 09:07:27 +0300

    security update

--- caseyazilim/settings.py
+++ caseyazilim/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4)#@tfw_ypd5#y$t^zd1m_$)w_%1ht_c)en8f)pj^fb2pinnkp'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
